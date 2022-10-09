d5c49b4689007215c19139e3e131b3d3b828b195
Roman Madyanov <romantaken@gmail.com>
2022-10-09 19:34:50 +0300

    Update microcode selection

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -2,20 +2,6 @@
 set -x
 set -euf -o pipefail
 
-# Select CPU
-
-PS3="Select option (1-3): "
-UCODE_PACKAGE=""
-select option in "Install AMD microcodes" "Install Intel microcodes" "Skip"
-do
-    case $REPLY in
-    1) UCODE_PACKAGE="amd-ucode"; break;;
-    2) UCODE_PACKAGE="intel-ucode"; break;;
-    3) break;;
-    *) echo "Invalid option"
-    esac
-done
-
 # System time
 
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
@@ -32,7 +18,6 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 read -p "Enter hostname (default 'arch'): " HOSTNAME
 HOSTNAME=${HOSTNAME:-"arch"}
-
 echo "$HOSTNAME" >> /etc/hostname
 
 echo "127.0.0.1	localhost" >> /etc/hosts
@@ -40,6 +25,18 @@ echo "::1		localhost" >> /etc/hosts
 echo "127.0.1.1	${HOSTNAME}" >> /etc/hosts
 
 # Packages
+
+PS3="Select option (1-3): "
+UCODE_PACKAGE=""
+select option in "Install AMD microcodes" "Install Intel microcodes" "Skip"
+do
+    case $REPLY in
+    1) UCODE_PACKAGE="amd-ucode"; break;;
+    2) UCODE_PACKAGE="intel-ucode"; break;;
+    3) break;;
+    *) echo "Invalid option"
+    esac
+done
 
 pacman -S --noconfirm networkmanager btrfs-progs sudo "$UCODE_PACKAGE"
 
@@ -60,18 +57,17 @@ echo "editor no" >> /boot/loader/loader.conf
 
 echo "title Arch Linux" >> /boot/loader/entries/arch.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
-
-if [[ -n $UCODE_PACKAGE ]]
+if [ -n "$UCODE_PACKAGE" ]
 then
     echo "initrd /${UCODE_PACKAGE}.img" >> /boot/loader/entries/arch.conf
 fi
-
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
 echo "options root=LABEL=ROOT rootflags=subvol=@ rw" >> /boot/loader/entries/arch.conf
 
 # User
 
-read -p "Enter user name: " USER
+read -p "Enter user name (default 'roman'): " USER
+USER=${USER:-"roman"}
 useradd -m -G wheel "$USER"
 passwd "$USER"
 
