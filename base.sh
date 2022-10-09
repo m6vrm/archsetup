97084115f608a49c095f177a5dc753a0c246a43e
Roman Madyanov <romantaken@gmail.com>
2022-10-09 23:32:31 +0300

    Fix bootloader options

--- base.sh
+++ base.sh
@@ -2,13 +2,9 @@
 set -x
 set -euf -o pipefail
 
-pause() {
-    read -p "Press Enter to continue..."
-}
-
 # Wizard
 
-. ./scripts/wizard.sh
+. ./wizards/base.sh
 
 # Unmount everything
 
@@ -20,48 +16,37 @@ timedatectl set-ntp true
 
 # Partitioning
 
-lsblk
-
 sgdisk --clear \
     --new=1:0:+1G   --typecode=1:ef00 \
     --new=2:0:0     --typecode=2:8300 \
-    "$rootdisk"
+    "$rootdevice"
 
-for disk in $pooldisks
+for device in $pooldevices
 do
-    sgdisk --clear --new=1:0:0 --typecode=1:8300 "$disk"
+    sgdisk --clear --new=1:0:0 --typecode=1:8300 "$device"
 done
-
-lsblk
-pause
 
 # Formatting
 
-efipart=$(lsblk -lnp -o name | grep "$rootdisk" | sed -n 2p)
-rootpart=$(lsblk -lnp -o name | grep "$rootdisk" | sed -n 3p)
+efipart=$(lsblk -lnp -o name | grep "$rootdevice" | sed -n 2p)
+rootpart=$(lsblk -lnp -o name | grep "$rootdevice" | sed -n 3p)
 
 mkfs.fat -F 32 -n EFI "$efipart"
 mkfs.btrfs -f -d single -L ROOT "$rootpart"
 
 mount "$rootpart" /mnt
 
-for disk in $pooldisks
+for device in $pooldevices
 do
-    diskpart=$(lsblk -lnp -o name | grep "$disk" | sed -n 2p)
+    diskpart=$(lsblk -lnp -o name | grep "$device" | sed -n 2p)
     btrfs device add -f "$diskpart" /mnt
 done
-
-lsblk
-pause
 
 # Mounting
 
 btrfs sub create /mnt/@
 btrfs sub create /mnt/@home
 umount /mnt
-
-lsblk
-pause
 
 # Mounting
 
@@ -70,9 +55,6 @@ mkdir /mnt/home
 mount -o noatime,space_cache=v2,compress=zstd:1,discard=async,subvol=@home "$rootpart" /mnt/home
 mkdir /mnt/boot
 mount "$efipart" /mnt/boot
-
-lsblk
-pause
 
 # Pacstrap
 
@@ -83,10 +65,12 @@ pacstrap /mnt base linux linux-firmware vim
 genfstab -U /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
 
-lsblk
-pause
-
 # Chroot
 
 cp ./scripts/chroot.sh /mnt/chroot.sh
-arch-chroot /mnt ./chroot.sh "$username" "$userpass" "$hostname" "$ucode"
+
+arch-chroot /mnt ./chroot.sh \
+    "$username" \
+    "$userpass" \
+    "$hostname" \
+    "$ucode"
