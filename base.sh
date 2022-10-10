7677f2078e4442279f573ca5f078283a91af561b
Roman Madyanov <romantaken@gmail.com>
2022-10-10 14:46:00 +0300

    Try to fix chroot.sh staring

--- base.sh
+++ base.sh
@@ -68,7 +68,7 @@ cat /mnt/etc/fstab
 
 cp "$(dirname "$0")/scripts/chroot.sh" /mnt/chroot.sh
 
-arch-chroot /mnt "bash /$(dirname "$0")/chroot.sh" \
+arch-chroot /mnt "$(dirname "$0")/scripts/chroot.sh" \
     "$username" \
     "$password" \
     "$hostname" \
